65a023555de53613f7fd3e87c5f2ec42e641c08c
Dan Taeyoung <dan@dantaeyoung.com>
2026-05-19 17:46:36 -0400

    Bump version tag to v260519-17:46

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
           <span v-if="currentBranch && currentBranch !== 'main'" class="branch-indicator">
             ({{ currentBranch }} branch)
           </span>
-          <span class="version-tag">v260519-16:53</span>
+          <span class="version-tag">v260519-17:46</span>
         </h1>
         <button class="tour-btn" @click="startTour" title="Take a guided tour">
           ?
